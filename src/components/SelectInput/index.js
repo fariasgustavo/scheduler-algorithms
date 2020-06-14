c616e1a0e69ba37794448e1cb407520fae171399
Gustavo Farias <devgustavofarias@gmail.com>
2020-06-14 03:08:46 -0300

    Create number input component

--- src/components/SelectInput/index.js
+++ src/components/SelectInput/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const SelectInput = () => {
-    const handleOnChange = value => {
+	const handleOnChange = (value) => {};
 
-    }
-
-    return (
-        <select onChange={handleOnChange}>
-            <option value='fifo'>FIFO</option>
-            <option value='round-robin'>Round-Robin</option>
-            <option value='sjf'>SJF</option>
-            <option value='prioridades'>Prioridades</option>
-        </select>
-    );
-}
+	return (
+		<select onChange={handleOnChange}>
+			<option value="fifo">FIFO</option>
+			<option value="round-robin">Round-Robin</option>
+			<option value="sjf">SJF</option>
+			<option value="prioridades">Prioridades</option>
+		</select>
+	);
+};
 
 export default SelectInput;
