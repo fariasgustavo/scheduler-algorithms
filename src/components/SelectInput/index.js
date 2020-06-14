18d996888aeeec55246d29f6b1fcc630955b3228
Gustavo Farias <devgustavofarias@gmail.com>
2020-06-14 03:00:54 -0300

    Start on select algorithm component

---
+++ src/components/SelectInput/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SelectInput = () => {
+    const handleOnChange = value => {
+
+    }
+
+    return (
+        <select onChange={handleOnChange}>
+            <option value='fifo'>FIFO</option>
+            <option value='round-robin'>Round-Robin</option>
+            <option value='sjf'>SJF</option>
+            <option value='prioridades'>Prioridades</option>
+        </select>
+    );
+}
+
+export default SelectInput;
